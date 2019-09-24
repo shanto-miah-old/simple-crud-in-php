9db07f645cb6a8fafc96dccec60984ea872cad8a
Shanto Miah <53362156+shanto-miah@users.noreply.github.com>
2019-09-24 09:23:50 +0600

    make responsive in mobile devices

--- add.php
+++ add.php
@@ -7,6 +7,8 @@
 	<meta charset="UTF-8">
 	<title>Simple CRUD</title>
 	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	<div class="container py-4">
