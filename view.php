c4cd33ab8ccc0854efe15d0702a95fdd2349bf97
Shanto Miah <53362156+shanto-miah@users.noreply.github.com>
2019-09-24 09:26:21 +0600

    make is responsive

--- view.php
+++ view.php
@@ -16,6 +16,8 @@
 	<meta charset="UTF-8">
 	<title>Simple CRUD</title>
 	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 	<div class="container py-4">
